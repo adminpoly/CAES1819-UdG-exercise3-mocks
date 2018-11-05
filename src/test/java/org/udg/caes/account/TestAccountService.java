3fbab4b7c713bbda5ab2309681518065214d2e37
Zheyi <1942301@campus.udg.edu>
2018-11-05 17:36:51 +0100

    Fet. Zheyi Liu

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -15,6 +15,24 @@ public class TestAccountService {
 
   @Test
   void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+
+      Account acc1 = new Account("a", 100);
+      Account acc2 = new Account("b", 350);
+
+      new Expectations() {{
+          am.findAccount("a"); result=acc1;
+          am.findAccount("b"); result=acc2;
+      }};
+      as.setAccountManager(am);
+      as.transfer("a", "b", 100);
+
+      assertEquals(0, acc1.getBalance());
+      assertEquals(450, acc2.getBalance());
+
+      new Verifications() {{
+          am.updateAccount(acc1); times = 1;
+          am.updateAccount(acc2); times = 1;
+      }};
+
   }
 }
